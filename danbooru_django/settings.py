5babff8349738c0ee728f06c0c1d7b16d1a8d6d6
Rahman Hakim <rahmanhakim2435@gmail.com>
2020-08-22 19:50:23 +0700

    Fixed several bugs and added information section and post count to dashboard

--- danbooru_django/settings.py
+++ danbooru_django/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'XXXX',
         'USER': 'XXXX',
-        'PASSWORD': 'XXXX',
+        'PASSWORD': 'xXXX',
     }
 }
 
